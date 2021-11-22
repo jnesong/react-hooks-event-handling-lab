9759d4a70daddd979cb4cdda7dd216036a08c068
Jenny Chau <jnechau@gmail.com>
2021-11-21 21:24:27 -0500

    commit tree 75424bcca0f8f798989d33cd5c0a9449c331bec3

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,14 @@
 // Code Keypad Component Here
+import React from "react";
+
+function Keypad (){
+    function passChange(){
+        console.log("Entering password...");
+    };//end of handleChange function
+
+    return (
+    <input type="password" onChange={passChange}></input>
+    );
+};
+
+export default Keypad
